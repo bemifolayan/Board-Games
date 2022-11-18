2b33564aed2fa114e5cb3df2169de13e2dc26621
BEMI FOLAYAN <gbemifolayan@wustl.edu>
2022-11-18 13:36:15 -0600

    commting 2

--- GomokuGame.cpp
+++ GomokuGame.cpp
@@ -4,8 +4,8 @@
 
 #include "GomokuGame.h"
 GomokuGame:: GomokuGame():GameBase(){
-    width = 19;
-    height = 19;
+    width = 21;
+    height = 21;
     maxlength = 2;
     trn = 0;
     p = "";
@@ -16,22 +16,22 @@ GomokuGame:: GomokuGame():GameBase(){
     }
 }
 
-bool GomokuGame:: done(){
+bool GomokuGame:: done() {
     //row
     int countB = 1;
     int countW = 1;
-    string track = "";
-    for(unsigned int i = rowOne; i <= height - 1; i++) {
-        for (unsigned int j = colOne; j <= width; j++) {
+
+    for (unsigned int i = rowOne; i <= height - 3; i++) {
+        for (unsigned int j = colOne; j <= width - 2; j++) {
             if (pieces.at(width * j + i).display_ == "B") {
-                if (pieces.at(width * (j + 1) + i).display_ == "B") {
+                if (pieces.at(width * j + (i + 1)).display_ == "B") {
                     countB++;
                     if (countB == 5)
                         return true;
                 } else
                     countB = 1;
             } else if (pieces.at(width * j + i).display_ == "W") {
-                if (pieces.at(width * (j + 1) + i).display_ == "W") {
+                if (pieces.at(width * j + (1 + i)).display_ == "W") {
                     countW++;
                     if (countW == 5)
                         return true;
@@ -40,59 +40,121 @@ bool GomokuGame:: done(){
             }
         }
     }
-        //columns
-        countB = 1;
-        countW = 1;
-        for(unsigned int i = rowOne; i <= height; i++){
-            for(unsigned int j = colOne; j <= width - 1; j++){
-                if(pieces.at(width* j+i).display_ == "B"){
-                    if(pieces.at(width* j + (i+1) ).display_ == "B"){
-                        countB++;
-                        if(countB == 5)
-                            return true;
-                    }
-                    else
-                        countB = 1;
-                }
-                else if(pieces.at(width* j+i).display_ == "W"){
-                    if(pieces.at(width* j+(i+1)).display_ == "W"){
-                        countW++;
-                        if(countW == 5)
-                            return true;
-                    }
-                    else
-                        countW = 1;
-                }
+    //columns
+    countB = 1;
+    countW = 1;
+    for (unsigned int i = rowOne; i <= height - 2; i++) {
+        for (unsigned int j = colOne; j < width - 3; j++) {
+            if (pieces.at(width * j + i).display_ == "B") {
+                if (pieces.at(width * (j + 1) + i).display_ == "B") {
+                    countB++;
+                    if (countB == 5)
+                        return true;
+                } else
+                    countB = 1;
+
+            } else if (pieces.at(width * j + i).display_ == "W") {
+                if (pieces.at(width * (j + 1) + i).display_ == "W") {
+                    countW++;
+                    if (countW == 5)
+                        return true;
+                } else
+                    countW = 1;
+            }
             }
         }
 
-        //diagonal going up to right
-        for(unsigned int i = rowOne; i <= 15; i++) {
-            for (unsigned int j = colOne; j <= 15; j++) {
-                if(pieces.at(width*j+i).display_ == "B" && pieces.at(width*j+i+20).display_ == "B" && pieces.at(width*j+i+40).display_ == "B"
-                && pieces.at(width*j+i+60).display_ == "B" && pieces.at(width*j+i+80).display_ == "B"){
-                    return true;
-                }
-                else if(pieces.at(width*j+i).display_ == "W" && pieces.at(width*j+i+20).display_ == "W" && pieces.at(width*j+i+40).display_ == "W"
-                       && pieces.at(width*j+i+60).display_ == "W" && pieces.at(width*j+i+80).display_ == "B"){
-                    return true;
-                }
-            }
-        }
-        //diagonal going up to left
+
+    //diagonal going up to right
+    /*
+
     for(unsigned int i = rowOne; i <= 15; i++) {
-        for (unsigned int j = 5; j <= 15; j++) {
-            if(pieces.at(width*j+i).display_ == "B" && pieces.at(width*j+i+18).display_ == "B" && pieces.at(width*j+i+36).display_ == "B"
-               && pieces.at(width*j+i+54).display_ == "B" && pieces.at(width*j+i+72).display_ == "B"){
+        for (unsigned int j = colOne; j <= 15; j++) {
+            if(pieces.at(width*j+i).display_ == "B" && pieces.at(width*j+i+20).display_ == "B" && pieces.at(width*j+i+40).display_ == "B"
+            && pieces.at(width*j+i+60).display_ == "B" && pieces.at(width*j+i+80).display_ == "B"){
                 return true;
             }
-            else if(pieces.at(width*j+i).display_ == "W" && pieces.at(width*j+i+18).display_ == "W" && pieces.at(width*j+i+36).display_ == "W"
-                    && pieces.at(width*j+i+54).display_ == "W" && pieces.at(width*j+i+72).display_ == "B"){
+            else if(pieces.at(width*j+i).display_ == "W" && pieces.at(width*j+i+20).display_ == "W" && pieces.at(width*j+i+40).display_ == "W"
+                   && pieces.at(width*j+i+60).display_ == "W" && pieces.at(width*j+i+80).display_ == "B"){
                 return true;
             }
         }
     }
+
+    //diagonal going up to left
+for(unsigned int i = rowOne; i <= 15; i++) {
+    for (unsigned int j = 5; j <= 15; j++) {
+        if(pieces.at(width*j+i).display_ == "B" && pieces.at(width*j+i+18).display_ == "B" && pieces.at(width*j+i+36).display_ == "B"
+           && pieces.at(width*j+i+54).display_ == "B" && pieces.at(width*j+i+72).display_ == "B"){
+            return true;
+        }
+        else if(pieces.at(width*j+i).display_ == "W" && pieces.at(width*j+i+18).display_ == "W" && pieces.at(width*j+i+36).display_ == "W"
+                && pieces.at(width*j+i+54).display_ == "W" && pieces.at(width*j+i+72).display_ == "B"){
+            return true;
+        }
+    }
+}*/
+
+
+    //diagonal going left
+
+    countB  = 1;
+    countW = 1;
+
+    for(unsigned int i = rowOne; i< width - 6; i++) {
+        for (unsigned int j = colOne; j < height - 6; j++) {
+            if(pieces.at(width *j + i).display_ == "B"){
+                for(unsigned int k = 1; k < 5; k++){
+                    if(pieces.at(width *(j+k) + (i+k) ).display_ == "B")
+                        countB++;
+                    else
+                        countB = 1;
+                }
+                if(countB == 5)
+                    return true;
+            }
+            if(pieces.at(width *i + j ).display_ == "W"){
+                for(unsigned int k = 1; k < 5; k++){
+                    if(pieces.at(width *(j+k) + (i+k) ).display_ == "W")
+                        countW++;
+                    else
+                        countW = 1;
+                }
+                if(countW == 5)
+                    return true;
+            }
+        }
+    }
+    countB = 1;
+    countW = 1;
+
+    for(unsigned int i = width-1; i > rowFour; i--){
+        for(unsigned int j = colOne; j < height-6; j++){
+            if(pieces.at(width *j + i ).display_ == "B"){
+                for(unsigned int k = 1;k < 5; k++){
+                        if(pieces.at(width *(j+k) + (i-k) ).display_ == "B") {
+                            countB++;
+                        }
+                        else
+                            countB = 1;
+                    if(countB == 5)
+                        return true;
+                }
+            }
+            if(pieces.at(width *i + j ).display_ == "W"){
+                for(unsigned int k = 1; k < 5; k++){
+                    if(pieces.at(width *(j+k) + (i-k) ).display_ == "W")
+                        countW++;
+                    else
+                        countW = 1;
+                }
+                if(countW == 5)
+                    return true;
+            }
+        }
+    }
     return false;
+
 }
 
 bool GomokuGame:: draw(){
@@ -123,9 +185,9 @@ int GomokuGame :: turn(){
     int promptVal = prompt( xcord, ycord);
     if(promptVal == Success){
         playCount++;
-        pieces.at(width* ycord + xcord).display_ = p;
-        pieces.at(width* ycord + xcord).xcoord_= xcord;
-        pieces.at(width* ycord + xcord).ycoord_ = ycord;
+        pieces.at(width* (ycord) + (xcord)).display_ = p;
+        pieces.at(width* (ycord) + (xcord)).xcoord_= xcord;
+        pieces.at(width* (ycord) + (xcord)).ycoord_ = ycord;
     }
     else if(promptVal == QuitGame){
         return QuitGame;
@@ -155,9 +217,9 @@ void GomokuGame::print(){
 ostream& operator<<(ostream& o, const GomokuGame & t) {
 
     o << setw(t.maxlength);
-    for (int i = t.height - 1; i >= 0; i--) {
-        o << i + 1;
-        for (unsigned int j = 0; j < t.width; j++) {
+    for (int i = t.height - 2; i >= 1; i--) {
+        o << i;
+        for (unsigned int j = 1; j <= t.width - 2; j++) {
             o << setw(t.maxlength) << t.pieces.at(t.width * i + j).display_ << " ";
         }
         o << endl;
@@ -165,7 +227,7 @@ ostream& operator<<(ostream& o, const GomokuGame & t) {
     o << "X" ;
 
 
-    for(unsigned int i = rowOne; i <= t.width; i++){
+    for(unsigned int i = rowOne; i <= t.width - 2; i++){
         o << setw(t.maxlength)<< i << " ";
     }
     o << endl;
